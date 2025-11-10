60fd9a7ec219e52719a67fdbb7a487c4799423ae
Agrim <agrimgupta31@gmail.com>
2025-11-10 22:15:58 +0530

    linting performed

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-def add(a,b):
-    return a+b
-
-def sub(a,b):
-    return a-b
+def add(a, b):
+    return a + b
 
 
+def sub(a, b):
+    return a - b
 
-res1=add(4,5)
-res2=sub(5,6)
 
-
-    
+res1 = add(4, 5)
+res2 = sub(5, 6)
